e9e8ffab1b3f1c436f012612557c42ab043b8e50
Liam Appleton <liamappleton97@outlook.com>
2025-01-17 22:51:48 +0000

    connected to mongodb

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const moongoose = require('mongoose');
+const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // replace with react url
+  })
+);
+app.use(express.json());
+
+const connectMongoose = async () => {
+  const port = process.env.PORT || 3000;
+  try {
+    await mongoose.connect(
+      `mongodb+srv://taskAdmin:${process.env.PASSWORD}@cluster0.fi5le.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+
+    console.log('Mongoose called...');
+
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+  } catch (error) {
+    console.log('Failed to connect ' + error);
+  }
+};
+
+connectMongoose();
