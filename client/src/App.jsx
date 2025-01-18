b2723d6079d1d75455bc4af3216612078be0a2fe
Liam Appleton <liamappleton97@outlook.com>
2025-01-18 10:29:42 +0000

    added form validation logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import TaskForm from './components/TaskForm';
 
 const App = () => {
+  const [formData, setFormData] = useState([]);
+
   return (
     <>
       <TaskForm />
--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -1,18 +1,51 @@
+import { useRef } from 'react';
+
 const TaskForm = () => {
+  const taskRef = useRef(null);
+  const descriptionRef = useRef(null);
+
+  const validateForm = ({ task, description }) => {
+    if (task.length < 3) return 'Task must be at least 3 characters.';
+    if (task.length > 3) return 'Task cannot be more than 50 characters.';
+    if (description.length < 10)
+      return 'Description must be at least 10 characters.';
+    if (description.length > 70)
+      return 'Description cannot be more than 70 characters.';
+    return 'valid';
+  };
+
+  const handleSubmit = () => {
+    const inputs = {
+      task: taskRef.current.value,
+      description: descriptionRef.current.value,
+    };
+  };
+
   return (
     <>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          handleSubmit();
+        }}
+      >
         <div className="mb-3">
           <label htmlFor="task" className="form-label">
             Task:{' '}
           </label>
-          <input name="task" type="text" className="form-control" />
+          <input ref={taskRef} id="task" type="text" className="form-control" />
         </div>
         <div className="mb-3">
           <label htmlFor="description" className="form-label">
             Description:{' '}
           </label>
-          <input name="description" type="text" className="form-control" />
+          <input
+            ref={descriptionRef}
+            id="description"
+            type="text"
+            className="form-control"
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Add
