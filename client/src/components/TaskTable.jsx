9f9aec1b54ce36b4929547b1983b03b4ae374d54
Liam Appleton <liamappleton97@outlook.com>
2025-01-18 23:19:01 +0000

    added conditionally rendered copy depending on if tasks are available or not

--- client/src/components/TaskTable.jsx
+++ client/src/components/TaskTable.jsx
@@ -6,35 +6,40 @@ const TaskTable = ({ unfinishedTasks, finishedTasks }) => {
       <div className="mb-3">
         <h1>To do</h1>
       </div>
-      <table className="table table-info">
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Description</th>
-            <th>Date due</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {unfinishedTasks.map((task) => {
-            return (
-              <tr key={task._id || task.title}>
-                <td>{task.title}</td>
-                <td>{task.description && task.description}</td>
-                <td>
-                  {task.dueDate && task.dueDate.match(/^\d{4}\-\d{2}\-\d{2}/)}
-                </td>
-                <td>
-                  <div>
-                    <button className="btn btn-success me-2">Done</button>
-                    <button className="btn btn-secondary">Edit</button>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {unfinishedTasks.length > 1 ? (
+        <table className="table table-info">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Description</th>
+              <th>Date due</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {unfinishedTasks.map((task) => {
+              return (
+                <tr key={task._id || task.title}>
+                  <td>{task.title}</td>
+                  <td>{task.description && task.description}</td>
+                  <td>
+                    {task.dueDate && task.dueDate.match(/^\d{4}\-\d{2}\-\d{2}/)}
+                  </td>
+                  <td>
+                    <div>
+                      <button className="btn btn-secondary me-2">Edit</button>
+                      <button className="btn btn-success">Done</button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        '✅ You’re all caught up—great job staying on top of things! 🌟'
+      )}
+
       <div className="mb-3">
         <h1>Completed</h1>
       </div>
@@ -70,7 +75,7 @@ const TaskTable = ({ unfinishedTasks, finishedTasks }) => {
           </tbody>
         </table>
       ) : (
-        'Chillax bro, nothing to do 🧉'
+        '🚀 No tasks completed yet—let’s get started! 💪'
       )}
     </>
   );
