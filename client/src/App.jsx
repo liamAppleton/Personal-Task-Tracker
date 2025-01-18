b394a3de83912dcad87e7ac52785007462442289
Liam Appleton <liamappleton97@outlook.com>
2025-01-18 12:24:09 +0000

    connected frontend to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TaskForm from './components/TaskForm';
+import TaskTable from './components/TaskTable';
+import axios from 'axios';
 
 const App = () => {
   const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/tasks')
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   const handleFormSubmission = (data) => {
     setFormData(data);
@@ -10,7 +19,12 @@ const App = () => {
 
   return (
     <>
-      <TaskForm getFormData={handleFormSubmission} />
+      <div className="mb-5">
+        <TaskForm getFormData={handleFormSubmission} />
+      </div>
+      <div>
+        <TaskTable />
+      </div>
     </>
   );
 };
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', // replace with react url
+    origin: 'http://localhost:5173', // replace with react url
   })
 );
 app.use(express.json());
