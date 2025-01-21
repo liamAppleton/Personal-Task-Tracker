98bb6b848122ed63cb1825ec79db4ab17bb4bd0b
Liam Appleton <liamappleton97@outlook.com>
2025-01-21 11:38:43 +0000

    added validation to form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,21 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const LoginForm = ({ userData }) => {
+  const [formData, setFormData] = useState({ username: '', password: '' });
+  const [error, setError] = useState('');
+
   useEffect(() => {
     console.log('Login form: ', userData);
   }, [userData]);
 
   const validateLogin = ({ username, password }) => {
-    let currentUser;
+    let index;
     for (let user of userData) {
       if (user.username === username) {
-        currentUser = user;
+        index = userData.indexOf(user);
       }
     }
 
-    if (currentUser.password !== password) return 'Invalid password';
+    if (userData[index].password !== password) {
+      setError('Invalid password');
+      return;
+    }
+
+    setError('');
     return 'valid';
   };
+
+  const handleSubmit = (e) => {};
 
   return (
     <>
@@ -23,13 +33,26 @@ const LoginForm = ({ userData }) => {
         <h3>Enter your login details</h3>
       </div>
 
-      <form className="form-floating">
+      <form
+        className="form-floating"
+        onSubmit={(e) => {
+          const validation = validateLogin(formData);
+          if (validation !== 'valid') {
+            e.preventDefault();
+          }
+        }}
+      >
         <div className="form-floating mb-3">
           <input
             type="text"
             className="form-control"
             id="floatingInput"
             placeholder="Username"
+            value={formData.username}
+            onChange={(e) => {
+              const updatedUserName = e.target.value;
+              setFormData({ ...formData, username: updatedUserName });
+            }}
           />
           <label htmlFor="floatingInput">Username</label>
         </div>
@@ -39,11 +62,19 @@ const LoginForm = ({ userData }) => {
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            value={formData.password}
+            onChange={(e) => {
+              const updatedPassword = e.target.value;
+              setFormData({ ...formData, password: updatedPassword });
+            }}
           />
           <label htmlFor="floatingInput">Password</label>
         </div>
-        <button className="btn btn-primary">Login</button>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
       </form>
+      {error && <p className="text-danger">{error}</p>}
     </>
   );
 };
